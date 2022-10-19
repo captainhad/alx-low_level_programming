b1990d5b77e98eb2eb5088c79fc7de3f25bb9ac9
captainhad <dawood.adam48@gmail.com>
2022-10-19 16:28:33 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main -entry point
+ *
+ * Return: Always 0 (success)
+ *
+ * description:print the _putchar
+ *
+ */
+
+int _putchar()
+{
+	char b;
+
+	b = "_putchar\n";
+	_putchar(%s, b);
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains lessons under functions, loops and nested loops under c programming language.

638d37d1f1922e70678c8dc5c101ab578d507c40
captainhad <dawood.adam48@gmail.com>
2022-10-20 05:28:07 -0700

    pushing main.h header file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
+#ifdef MAIN_H_
+#defind MAIN_H_
+
+int _putchar(char);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int isalpha(int c);
+int print_sign(int n);
+int _abs(int n);
+int print_last_digit(int);
+void jack_bauer(void);
+void time_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
+#endif

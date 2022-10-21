d99e5ba8a0cb80f1b8f2e5254599aaf92d5435d3
captainhad <dawood.adam48@gmail.com>
2022-10-20 17:46:56 -0700

    is alpha

--- 0x02-functions_nested_loops/isalpha.c
+++ 0x02-functions_nested_loops/isalpha.c
@@ -15,7 +15,7 @@ int _isalpha(int c)
 
 	for (lower = 'a'; lower <= 'z'; lower++)
 	{
-		for (upper = 'A'; upper <= 'Z'; upper++0)
+		for (upper = 'A'; upper <= 'Z'; upper++)
 		{
 			if (c == lower || c == upper)
 				isletter = 1;

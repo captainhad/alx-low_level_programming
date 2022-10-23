9dfedbb2ce54ecadf02e659077c1ebadb14f043f
captainhad <dawood.adam48@gmail.com>
2022-10-22 19:38:12 -0700

    print times table of nine

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_times_table - Check Description
+ * Description: print times table for 9 starting from zero
+ * Return: Nothing.
+ */
+
+void print_times_table(int n)
+{
+	int i, j;
+
+	if (n > 0 && n < 10)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+				_putchar(i * j);
+			_putchar('\n');
+		}
+	}
+}

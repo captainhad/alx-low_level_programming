ddcf815ed3a17bb33413d3aade72c72e14cfe706
captainhad <dawood.adam48@gmail.com>
2022-10-25 17:21:40 -0700

    Reserving a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - A funtion that print a string in reverse 
+ * , followed by a new line
+ * @s: An input string
+ * Return: Nothing.
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (len)
+		_putchar(s[--len]);
+	_putchar('\n');
+}

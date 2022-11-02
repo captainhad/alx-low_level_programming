89be34673fb10b2e5142f7eb1ee1c86cf2e8c56e
captainhad <dawood.adam48@gmail.com>
2022-11-02 16:55:57 -0700

    A function that returns the natural square roots

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,7 +2,7 @@
 
 
 /**
- * _sqrt_recursion -A function that returns tha natural 
+ * _sqrt_recursion -A function that returns tha natural
  * square root of a number
  * @n: An input integer
  * Return: the square root of the integer n
@@ -17,13 +17,4 @@ int _sqrt_recursion(int n)
 		return (n);
 
 	return (_sqrt_recursion(n));
-/*
-	int _sqrt(int prev, int root)
-	{
-		if (prev > root)
-			return (-1);
-		else if (prev * prev == root)
-			return (prev);
-		return (_sqrt(prev + 1, root));
-	}*/
 }

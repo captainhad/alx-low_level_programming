302c143d61d913428cf06f062af33357ef7f9f1a
captainhad <dawood.adam48@gmail.com>
2022-11-19 14:37:19 -0800

    A program of multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - A test function that prints if an
+ * integer is positive or negative
+ * Retuen: 0 as success
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}

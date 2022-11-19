72ea9079fcf83b43255cd3aa53168314deb2327f
captainhad <dawood.adam48@gmail.com>
2022-11-19 15:15:33 -0800

    A program that causes infinite loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - A program that causes an infinite loop
+ * Return: 0 as success
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("infinite loop incoming :(\n");
+
+	i = 0;
+
+	/* while (i < 10)*/
+	/* {*/
+	/*	putchar(i);*/
+	/* }*/
+
+	printf("infinite loop avoided! \\o/\n");
+
+	return (0);
+}
